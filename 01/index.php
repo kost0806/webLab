8966bbebf247222d07a86dfc4aef093df9c6b528
kostrian <kost0806@naver.com>
2015-09-16 16:54:50 +0900

    ll

---
+++ 01/index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="http://cdclconsult.com/wp-content/uploads/2014/07/no-img.jpg" type="image/jpg" rel="shortcut icon">
+	<title>Resume</title>
+</head>
+<body>
+	<h1>Kim Minyoung</h1>
+	<img src="http://cdclconsult.com/wp-content/uploads/2014/07/no-img.jpg" alt="me" width="200" height="200">
+	<ul>
+		<li>6, Seoksu-ro, Danwon-gu, Ansan-si, Gyeonggi-do</li>
+		<li>kost0806@gmail.com</li>
+		<li>010-4658-7626</li>
+	</ul>
+	<hr>
+	<h3>Objective</h3>
+	<p>My overall objective is to improve my <em>programming</em> skills and <em>security</em> skills</p>
+	
+	<h3>Experience</h3>
+	<ul>
+		<li>Intern at Softopia in Hanyang Univ. ERICA<br>2015.01 ~ 2015.03</li>
+		<li>Intern at Chungwoo Funstation<br>2015.06 ~ 2015.08</li>
+	</ul>
+	
+	<h3>Skills</h3>
+	<ol>
+		<li>
+			Programming Languages
+			<ul>
+				<li>
+					System
+					<ul>
+						<li>C</li>
+						<li>Shell Script</li>
+					</ul>
+				</li>
+				<li>
+					Application
+					<ul>
+						<li>Java</li>
+						<li>Python</li>
+					</ul>
+				</li>
+				<li>
+					Web Application
+					<ul>
+						<li>HTML</li>
+						<li>CSS</li>
+						<li>PHP</li>
+					</ul>
+				</li>
+				<li>
+					Mobile Application
+					<ul>
+						<li>Android</li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+		<li>
+			Tools
+			<ul>
+				<li>Visual Studio</li>
+				<li>Eclise</li>
+				<li>Intelij</li>
+				<li>PyCharm</li>
+				<li>Ollydbg</li>
+				<li>GDB</li>
+				<li>Vim</li>
+			</ul>
+		</li>
+	</ol>
+	
+	<h3>Education</h3>
+	<ul>
+		<li>2010, <a href="www.kirin.ms.kr">Ki-rin Middleschool</a>, Korea</li>
+		<li>2013, <a href="www.jeonil.hs.kr">Jeon-il Highschool</a>, Korea</li>
+		<li>2013 ~, <a href="http://www.hanyang.ac.kr/">Hanyang Univ. ERICA Camp.</a>, Korea</li>
+	</ul>
+	<hr>
+	<h3>My Favorite Movies</h3>
+	<table >
+		<caption>My favorite movie list</caption>
+		<tr>
+			<th>Number</th>
+			<th>Title</th>
+			<th>Year</th>
+			<th>Genre</th>
+			<th>Director</th>
+			<th>Actor</th>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>Fase & Furios 3</td>
+			<td>2006</td>
+			<td>Race</td>
+			<td>Justin Lin</td>
+			<td>Lucas Black, Bow Wow, Sung Kang</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td>Inception</td>
+			<td>2010</td>
+			<td>Action, Mystery, Sci-Fi</td>
+			<td>Christopher Nolan</td>
+			<td>Leonardo DiCaprio, Ellen Page</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td>Interstellar</td>
+			<td>2014</td>
+			<td>Sci-Fi</td>
+			<td>Christopher Nolan</td>
+			<td>Matthew McConaughey, Anne Hathaway</td>
+		</tr>
+	</table>
+	
+	<h3>Movie Trailer from Youtube</h3>
+	<p>STAR WARS Episode 7 TRAILER # 2</p>
+	<object width="560" height="315" type="application/x-shockwave-flash" data="https://www.youtube.com/v/clLYRvtsoZ0">
+		<param name="wmode" value="transparent">
+	</object>
+	
+	<h3>One of My Favorite Songs</h3>
+	<p>Lost stars by Adam Levine</p>
+	<audio src="lost_stars.mp3" controls="controls" preload="auto"></audio>
+	
+	<h3>My Favorite Quote</h3>
+	<blockquote>
+		"Stay hungry. Stay foolish."<br>
+		&ndash;<em>Steve Jobs</em>
+	</blockquote>
+	<h3>Short Quiz</h3>
+	<dl>
+		<dt><abbr title="Domain Name System">DNS</abbr></dt><dd>The DNS is a hierarchical distributed naming system for computers, services, or any resource connected to the Internet or a private network.</dd>
+		<dt><abbr title="Maximum Transmission Unit">MTU</abbr></dt><dd>The size of the largest packet that a network protocol can transmit</dd>
+		<dt><abbr title="Internet Service Provider">ISP</abbr></dt><dd>An ISP is an organization that provides services for accessing, using, or participating in the Internet.</dd>
+	</dl>
+	
+	<a href="http://validator.w3.org/check/referer">
+		<img src="http://selab.hanyang.ac.kr/courses/cse326/2015/problems/images/w3c-html.png" alt="Validate!">
+	</a>
+	<a href="http://jigsaw.w3.org/css-validator/check/referer">
+		<img src="http://selab.hanyang.ac.kr/courses/cse326/2015/problems/images/w3c-css.png" alt="Validate!">
+	</a>
+</body>
+</html>
